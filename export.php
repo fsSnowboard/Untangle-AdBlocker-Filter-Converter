eea6f4f04c523ff2bca1855401582fdb7ed1c3be
Tyler Turner <tyler@Tylers-MacBook-Pro.local>
2013-03-29 21:45:01 -0400

    all modifications to original script

--- export.php
+++ export.php
@@ -1,11 +1,130 @@
-<?php 
+<?php
+/* ********************************************* */
+/* Original script by WebFooL on Untangle Forums */
+/* Modifications by fsSnowboard                  */
+/* ********************************************* */
+
 //Downloaded from http://msimmons.ws/?p=142 which is based on
 //http://forums.untangle.com/ad-blocker/29204-convert-adblock-plus-txt-file-json-format-4.html
 
-$Content = "./easylist.txt";  
-$lines = file($Content);  
-$linesSplit = array_chunk( $lines, 2000 ); 
-$badcharacters = array("#",'"', "'", "[", "]", "\n", "\t", "\r");  
+//https://easylist-downloads.adblockplus.org/easylist_noelemhide.txt
+//https://adblockplus.org/en/filters#options
+//https://adblockplus.org/en/filter-cheatsheet
+//http://jsonlint.com/
+//setup auto download of new file and check file expireation before downloading another copy
+//AdBlocker appears to only black ads based on a match of strings in the URL
+
+$Content = "./easylist_noelemhide.txt";
+$timezone_adjust = 60 * 60 * 7;
+$lines = file($Content);
+
+$last_modified =  preg_replace('/! Last modified: /', '', $lines[3]);
+$last_modified_time = strtotime($last_modified);
+echo "File last modified: ". $last_modified ." (".$last_modified_time.")</br>\n";
+
+$date_match_time = time() - (60 * 60 * 24 * 5) + $timezone_adjust;
+echo "Date Match time (only used for matching if last modified is less than current date): ". $download_date_match ."(".$date_match_time.")</br>\n";
+
+$download_match_time = $last_modified_time + (60 * 60 * 24 * 5) + $timezone_adjust;
+$download_date_match = date("d M Y H:i T", $date_match_time);
+
+echo "New download on: ". date("d M Y H:i", $download_match_time) ." (".$download_match_time.")</br>\n";
+
+if($last_modified_time <= $date_match_time) {
+	echo "File is 5 days old, downloading new one.";
+	//if file is 5 days old, download new one
+	$new_easylist = file_get_contents("https://easylist-downloads.adblockplus.org/easylist_noelemhide.txt");
+	file_put_contents("easylist_noelemhide.txt", $new_easylist);
+	
+	//read new file
+	$Content = "./easylist_noelemhide.txt";  
+	$lines = file($Content);
+}
+
+
+
+$badcharacters = array("#", '"', "'", "[", "]", "^", "\n", "\t", "\r", "||");
+
+
+unset($lines[0]);  //Line 0 is [Adblock Plus 2.0] and not needed
+
+//echo "test".strpos('007-gateway.com$third-party', '$')."\n";
+//foreach ($lines as $key => $value) {
+//    if($value{0} == "!") {
+//    	unset($lines[$key]);
+//    }
+//}
+//$lines = array_values($lines);  //repair index
+//print_r($lines);
+
+//
+foreach ($lines as $key => $value) {
+	//strip out lines begining with ! because they are comments
+    if($value{0} == "!") {
+    	unset($lines[$key]);
+    }
+}
+
+//remove line with @@ which are pass rules
+foreach ($lines as $key => $value) {
+    if($value{0} == "@" && $value{1} == "@") {
+    	unset($lines[$key]);
+    }
+}
+
+//foreach ($lines as $key => $value) {
+	
+    //strip everything to the right and including ^
+    //$pos = strpos($value, "^");
+	//if ($pos !== FALSE) {
+	//    //echo $key." ".$pos."\n";
+	//	//echo "part:". substr($value, 0, $pos)."\n";
+	//	//echo "full:".$value."\n";
+    //	$lines[$key] = substr($value, 0, $pos);
+    //}
+//}
+
+//strip everything to the right of $
+foreach ($lines as $key => $value) {
+    $pos = strpos($value, '$');
+    if ($pos !== FALSE) {
+	    //echo $key." ".$pos."\n";
+		//echo "part:". substr($value, 0, $pos)."\n";
+		//echo "full:".$value."\n";
+		$lines[$key] = substr($value, 0, $pos);
+	}
+}
+
+//Remove empty lines
+foreach ($lines as $key => $value) {
+    if($value == "") {
+    	unset($lines[$key]);
+    }
+}
+
+//$filtered = array();
+//foreach ($lines as $key => $value){
+//    if(in_array($value, $filtered)){
+//        continue;
+//    }
+//    array_push($filtered, $value);
+//}
+
+//print_r($filtered);
+
+//strip out bad characters
+foreach ($lines as $key => $value) {
+	$lines[$key] = str_replace($badcharacters, "", $value);
+}
+
+$lines = array_values(array_unique($lines));
+//$lines = array_unique($lines);  //eliminate duplicates
+//$lines = array_values($lines);  //repair index
+//print_r($lines);
+
+$linesSplit = array_chunk( $lines, 2000 );
+print_r($linesSplit);
+
 
 $i = 0; 
 foreach ($linesSplit as $inner_array) { 
@@ -14,13 +133,13 @@ foreach ($linesSplit as $inner_array) {
     $filestart = "[";  
     fwrite($fp, $filestart); 
     while (list($key, $value) = each($inner_array)) 
-    {     
-        $cleanstr = str_replace($badcharacters, "", $value);     
-    $store = '{"enabled":true,"string":"'.$cleanstr.'","javaClass":"com.untangle.uvm.node.GenericRule"},';  
-    fwrite($fp, $store); 
-    }  
+    {
+        //$cleanstr = str_replace($badcharacters, "", $value);     
+    	$store = '{"enabled":true,"string":"'.$value.'","javaClass":"com.untangle.uvm.node.GenericRule"},';
+		fwrite($fp, $store);
+    }
     $fileend = "]";  
     fwrite($fp, $fileend); 
-    fclose($fp); 
-} 
+    fclose($fp);
+}
 ?>
